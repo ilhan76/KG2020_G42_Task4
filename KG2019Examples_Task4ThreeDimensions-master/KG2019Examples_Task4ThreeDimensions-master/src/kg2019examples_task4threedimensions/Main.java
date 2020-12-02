e3c4d845af444f0da7f52a10de3dcaaab2b67baf
ilhan76 <71686887+ilhan76@users.noreply.github.com>
2020-12-02 16:16:22 +0300

    Add files via upload

---
+++ KG2019Examples_Task4ThreeDimensions-master/KG2019Examples_Task4ThreeDimensions-master/src/kg2019examples_task4threedimensions/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kg2019examples_task4threedimensions;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.*;
+
+/**
+ *
+ * @author Alexey
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(new DrawPanel());
+        frame.setVisible(true);
+    }
+}
